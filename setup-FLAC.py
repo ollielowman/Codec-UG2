9f09fd32f03f85209e1414d48e700d58cbdc5cb2
Stinky <mykastudent@gmail.com>
2024-09-23 22:22:17 +0930

    fixed dependencies download/installation

--- setup-FLAC.py
+++ setup-FLAC.py
@@ -19,6 +19,8 @@ def download_dependencies():
                 "scipy",
                 "pyflac",
                 "librosa",
+                "setuptools",
+                "wheel"
             ]
         )
         print(
@@ -47,6 +49,8 @@ def install_dependencies():
                 "scipy",
                 "pyflac",
                 "librosa",
+                "setuptools",
+                "wheel"
             ]
         )
         print(
